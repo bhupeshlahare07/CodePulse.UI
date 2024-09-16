2b1b9805fe7c859654fe0ee3e009b6fbacc05c36
bhupeshlahare <bhupeshlahare@gmail.com>
2024-09-16 21:00:24 +0530

    add category ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Core/Components/navbar/navbar.component';
+import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
+import { AddCategoryComponent } from './Features/Category/add-category/add-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule,
