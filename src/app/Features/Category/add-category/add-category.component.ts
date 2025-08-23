f76f4166ca11687f48e799e2b6657f790fb834c1
bhupeshlahare <bhupeshlahare@gmail.com>
2025-08-23 18:06:21 +0530

    Unsubscribing AddCategoryComponent

--- src/app/Features/Category/add-category/add-category.component.ts
+++ src/app/Features/Category/add-category/add-category.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../Services/category.service';
 import { response } from 'express';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
-export class AddCategoryComponent {
+/* The AddCategoryComponent class in TypeScript defines a model for adding a category and includes a
+method for submitting the form data to the CategoryService. */
+/* This class is named AddCategoryComponent and implements the OnDestroy interface. */
+export class AddCategoryComponent implements OnDestroy {
 
   model:AddCategoryRequest;
+  private addCategorySubscription?:Subscription;
 
   constructor(private CategoryService: CategoryService){
     this.model={
@@ -18,14 +23,22 @@ export class AddCategoryComponent {
       urlHandel:''
     };
   }
+  
   onFormSubmit()
   {
     //console.log(this.model);
-    this.CategoryService.addCategory(this.model)
+    this.addCategorySubscription = this.CategoryService.addCategory(this.model)
     .subscribe({
       next : (response) => {
         console.log('This was successfull')
       }
     })
+  }
+
+  /**
+   * The ngOnDestroy function in TypeScript unsubscribes from the addCategorySubscription if it exists.
+   */
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
